a23dcc64a9838aabc820d474d0707153700fc138
JuhoSoivio <soivio.juho@gmail.com>
2026-02-11 13:16:20 +0200

    Revert "Merge branch 'JuhoS'"
    This reverts commit a5db275854f955d2d411cbb44ea47a5c5f8268c7, reversing
    changes made to 9dec5c59cbab6f97d0ed00ae44c310d8dd5daacf.

--- Assets/Scripts/UI/AmmoCounter.cs
+++ Assets/Scripts/UI/AmmoCounter.cs
@@ -1,110 +1,48 @@
-//using UniRx;
-//using UnityEngine;
-//using TMPro;
-//using TopDown.Shooting;
-//using System.Runtime.CompilerServices;
-//using static UnityEngine.Rendering.DebugUI;
-
-//namespace TopDown.UI
-//{
-
-//    public class NewMonoBehaviourScript : MonoBehaviour
-//    {
-//        private CompositeDisposable subscriptions = new CompositeDisposable();
-
-//        [SerializeField] private TextMeshProUGUI ammoCounterText;
-//        [SerializeField] private WeaponController weaponController;
-
-//        private int ammoInClip;
-//        private int totalAmmo;
-
-
-//        private void OnEnable()
-//        {
-//            weaponController.CurrentAmmoInClip.ObserveEveryValueChanged(property => property.Value).Subscribe(value =>
-//            {
-//                ammoInClip = value;
-//                UpdateAmmoCounter(ammoInClip, totalAmmo);
-//            }).AddTo(subscriptions);
-
-
-//            weaponController.TotalAmmo.ObserveEveryValueChanged(property => property.Value).Subscribe(value =>
-//            {
-//                totalAmmo = value;
-//                UpdateAmmoCounter(ammoInClip, totalAmmo);
-//            }).AddTo(subscriptions);
-//        }
-
-//        private void OnDisable()
-//        {
-//            subscriptions.Clear();
-//        }
-
-//        private void UpdateAmmoCounter(int currentAmmo, int totalAmmo)
-//        {
-//            ammoCounterText.text = $"{currentAmmo}/{totalAmmo}";
-//        }
-//    }
-//}
-
 using UniRx;
 using UnityEngine;
 using TMPro;
 using TopDown.Shooting;
+using System.Runtime.CompilerServices;
+using static UnityEngine.Rendering.DebugUI;
 
 namespace TopDown.UI
 {
-    public class AmmoCounter : MonoBehaviour
+
+    public class NewMonoBehaviourScript : MonoBehaviour
     {
         private CompositeDisposable subscriptions = new CompositeDisposable();
 
-        [Header("UI Components")]
         [SerializeField] private TextMeshProUGUI ammoCounterText;
-
-        [Header("Weapon Source")]
         [SerializeField] private WeaponController weaponController;
 
-        private int _ammoInClip;
-        private int _totalAmmo;
+        private int ammoInClip;
+        private int totalAmmo;
+        
 
         private void OnEnable()
         {
-            if (weaponController == null)
+            weaponController.CurrentAmmoInClip.ObserveEveryValueChanged(property => property.Value).Subscribe(value =>
             {
-                Debug.LogWarning($"AmmoCounter on {gameObject.name} is missing a WeaponController reference!");
-                return;
-            }
+                ammoInClip = value;
+                UpdateAmmoCounter(ammoInClip, totalAmmo);
+            }).AddTo(subscriptions);
 
-            // Subscribe directly to the ReactiveProperties
-            weaponController.CurrentAmmoInClip
-                .Subscribe(value =>
-                {
-                    _ammoInClip = value;
-                    UpdateUI();
-                })
-                .AddTo(subscriptions);
 
-            weaponController.TotalAmmo
-                .Subscribe(value =>
-                {
-                    _totalAmmo = value;
-                    UpdateUI();
-                })
-                .AddTo(subscriptions);
+            weaponController.TotalAmmo.ObserveEveryValueChanged(property => property.Value).Subscribe(value =>
+            {
+                totalAmmo = value;
+                UpdateAmmoCounter(ammoInClip, totalAmmo);
+            }).AddTo(subscriptions);
         }
 
         private void OnDisable()
         {
-            // This prevents memory leaks by disposing of all subscriptions at once
             subscriptions.Clear();
         }
 
-        private void UpdateUI()
+        private void UpdateAmmoCounter(int currentAmmo, int totalAmmo)
         {
-            if (ammoCounterText != null)
-            {
-                ammoCounterText.text = $"{_ammoInClip}/{_totalAmmo}";
-            }
+            ammoCounterText.text = $"{currentAmmo}/{totalAmmo}";
         }
     }
 }
