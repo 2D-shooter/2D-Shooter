db30d3b9ccaedea1c10264486268aa004db5a3ef
Aake Moskuvaara <t9moaa00@students.oamk.fi>
2026-03-19 14:22:42 +0200

    optionssit ja valikot toimii

---
+++ Assets/Scripts/Managers/SoundMixerManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundMixerManager : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+
+    public void SetMasterVolume(float level)
+    {
+        //level = Mathf.Clamp(level, 0.0001f, 1f);
+        audioMixer.SetFloat("mastervolume", Mathf.Log10(level) *20f);
+    }
+
+    public void SetSoundFXVolume(float level)
+    {
+        //level = Mathf.Clamp(level, 0.0001f, 1f);
+        audioMixer.SetFloat("soundfxvolume", Mathf.Log10(level) *20f);
+        SoundFXManager.Instance.PlayShoot();
+    }
+
+    public void SetMusicVolume(float level)
+    {
+        //level = Mathf.Clamp(level, 0.0001f, 1f);
+        audioMixer.SetFloat("musicvolume", Mathf.Log10(level) *20f);
+    }
+}
